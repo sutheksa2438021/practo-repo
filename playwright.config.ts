6166c0f1664fe627b7e81cdfd8351f1bec5b50f7
sutheksa2438021 <Sutheksa.P@cognizant.com>
2025-10-09 09:25:44 +0530

    commit

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,15 +2,16 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
-  timeout: 30 * 1000,
+  timeout: 0,
   retries: 0,
   use: {
     headless: true,
     viewport: { width: 1280, height: 720 },
     ignoreHTTPSErrors: true,
-    video: 'retain-on-failure',
-    screenshot: 'only-on-failure',
+    video: 'on',
+    screenshot: 'on',
   },
+  reporter: [['allure-playwright']],
   projects: [
     {
       name: 'Chromium',
